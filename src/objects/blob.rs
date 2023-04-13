144c0e2e476068ec75a21e35dd785facc2840952
henri <henri@moosbauer.com>
2023-04-13 19:25:43 +0200

    use ciborium for blob serialization

--- src/objects/blob.rs
+++ src/objects/blob.rs
@@ -1,9 +1,12 @@
 use crate::{storing::Storable, Paths};
+use ciborium;
+use serde::{Deserialize, Serialize};
 use sha1_smol;
 use std::{fmt, fs, path};
 
 const BLOB_IDENTIFIER: &str = "blob ";
 
+#[derive(Serialize, Deserialize)]
 pub struct Blob {
     bytes: Vec<u8>,
     sha1: String,
@@ -30,15 +33,13 @@ impl Blob {
     }
 
     pub fn new_from_object_file(sha1: String) -> Blob {
-        let buf = fs::read(Paths::objects() + &sha1)
-            .expect(&format!("Failed to read {} to create blob", sha1));
+        let file = fs::File::open(Paths::objects() + "/" + &sha1)
+            .expect(&format!("Failed to open {} to create blob", sha1));
+
+        let buf: Vec<u8> = ciborium::de::from_reader(file).expect("Failed to read data from blob");
 
         let bytes = &buf[BLOB_IDENTIFIER.len()..];
         let blob = Blob::from_bytes(bytes);
-
-        if blob.sha1().ne(&sha1) {
-            panic!("Filename sha != content sha");
-        }
 
         blob
     }
@@ -53,7 +54,9 @@ impl Storable for Blob {
         let mut buf: Vec<u8> = BLOB_IDENTIFIER.into();
         buf.extend_from_slice(&self.bytes);
 
-        fs::write(Paths::objects() + "/" + &self.sha1, &buf)
-            .expect(&format!("Failed to store blob"));
+        let file = fs::File::create(Paths::objects() + "/" + &self.sha1)
+            .expect("Failed to create file to store blob");
+
+        ciborium::ser::into_writer(self, file).expect("Failed to write data to blob");
     }
 }
