b71749c2402e9a452adff859dffcce98d96706ba
henri <henri@moosbauer.com>
2023-05-26 22:23:07 +0200

    remove unnecessary deps

--- src/storing.rs
+++ src/storing.rs
@@ -1,5 +1,3 @@
-use serde::{Deserialize, Serialize};
-
 pub trait Storable {
     fn store(&self);
 }
@@ -8,30 +6,4 @@ pub trait Object {
     fn new_from_object_file(sha: &str, name: Option<String>) -> Self;
 
     fn serialize(&self) -> Vec<u8>;
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Finalized<T>
-where
-    T: Storable,
-{
-    value: T,
-}
-
-impl<T> Finalized<T>
-where
-    T: Storable,
-{
-    pub fn new(value: T) -> Finalized<T> {
-        Finalized { value }
-    }
-}
-
-impl<T> Storable for Finalized<T>
-where
-    T: Storable,
-{
-    fn store(&self) {
-        self.value.store()
-    }
 }
